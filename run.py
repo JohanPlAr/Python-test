84a246993be3aafeafe4e63f7fae07ec3ea7e9b1
Johan Plym Arkert <johan.arkert@gmail.com>
2023-09-05 12:31:51 +0200

    CharacterBuilder and addStatpoints

--- run.py
+++ run.py
@@ -1,3 +1,140 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random, pprint, os, time
+
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def menu():
+    print("Choose your opponent")
+
+
+class CharacterStats:
+    """
+    The object collects the character stats
+    """
+
+    def __init__(self, type, name, strengthPoints, healthPoints, skillPoints, armor):
+        self.type = type
+        self.name = name
+        self.strengthPoints = strengthPoints
+        self.healthPoints = healthPoints
+        self.skillPoints = skillPoints
+        self.armor = armor
+
+    def __str__(self):
+        return f"{self.name.upper()} THE MIGHTY {self.type.upper()}\nSTRENGTH:\t{self.strengthPoints}\nHEALTH:\t\t{self.healthPoints}\nSWORD SKILL:\t{self.skillPoints}\nARMOR:\t\t{self.armor}"
+
+
+def dice(num):
+    result = 0
+    total = 0
+    for i in range(num):
+        result = random.randint(1, 6)
+        total += result
+    return total
+
+
+def characterInput():
+    name = input("Choose your characters name\n")
+    time.sleep(1)
+    clear_screen()
+    type = input("Choose your characters type\nHuman/Elf/Dwarf/Orc\n").lower()
+    time.sleep(1)
+    clear_screen()
+    if type == "human":
+        strengthPoints = 15 + dice(6)
+        healthPoints = 15 + dice(6)
+        skillPoints = 15 + dice(6)
+        armor = 5
+        statsPoints = dice(10)
+        player = CharacterStats(
+            type, name, strengthPoints, healthPoints, skillPoints, armor
+        )
+    if type == "elf":
+        strengthPoints = 15 + dice(6)
+        healthPoints = 15 + dice(6)
+        skillPoints = 15 + dice(6)
+        armor = 5
+        statsPoints = dice(9)
+        player = CharacterStats(
+            type, name, strengthPoints, healthPoints, skillPoints, armor
+        )
+    if type == "dwarf":
+        strengthPoints = 15 + dice(6)
+        healthPoints = 15 + dice(6)
+        skillPoints = 15 + dice(6)
+        armor = 5
+        statsPoints = dice(10)
+        player = CharacterStats(
+            type, name, strengthPoints, healthPoints, skillPoints, armor
+        )
+    if type == "orc":
+        strengthPoints = 15 + dice(6)
+        healthPoints = 15 + dice(6)
+        skillPoints = 15 + dice(6)
+        armor = 5
+        statsPoints = dice(10)
+        player = CharacterStats(
+            type, name, strengthPoints, healthPoints, skillPoints, armor
+        )
+    else:
+        print(f"Choices available are Human/Elf/Dwarf/Orc\nYou wrote {name}")
+
+    print(player)
+    addStatsPoints(player, statsPoints)
+    return player
+
+
+def addStatsPoints(player, statsPoints):
+    while True:
+        time.sleep(2)
+        clear_screen()
+        print(f"You have {statsPoints} points to add to your stats")
+        print(
+            f"What stats would you like to improve?\n1. STRENGTH:\t{getattr(player, 'strengthPoints')}\n2. HEALTH:\t{getattr(player, 'healthPoints')}\n3. SWORD SKILL:\t{getattr(player, 'skillPoints')}\n4. ARMOR:\t{getattr(player, 'armor')}\n"
+        )
+        selectAttribute = input(f"Choose attribute 1/2/3/4: ")
+        if selectAttribute == "1":
+            activateStatPoints = int(input(f"How many points do you wish to add"))
+            if activateStatPoints <= statsPoints:
+                player.strengthPoints += activateStatPoints
+                statsPoints -= activateStatPoints
+            else:
+                print(f"Not enough points left\nYou have {statsPoints} left")
+                addStatsPoints(player, statsPoints)
+        if selectAttribute == "2":
+            activateStatPoints = int(input(f"How many points do you wish to add: "))
+            if activateStatPoints <= statsPoints:
+                player.healthPoints += activateStatPoints
+                statsPoints -= activateStatPoints
+            else:
+                print(f"Not enough points left\nYou have {statsPoints} left")
+                addStatsPoints(player, statsPoints)
+        if selectAttribute == "3":
+            activateStatPoints = int(input(f"How many points do you wish to add: "))
+            if activateStatPoints <= statsPoints:
+                player.skillPoints += activateStatPoints
+                statsPoints -= activateStatPoints
+            else:
+                print(f"Not enough points left\nYou have {statsPoints} left")
+                addStatsPoints(player, statsPoints)
+        if selectAttribute == "4":
+            activateStatPoints = int(input(f"How many points do you wish to add: "))
+            if activateStatPoints <= statsPoints:
+                player.armor += activateStatPoints
+                statsPoints -= activateStatPoints
+            else:
+                print(f"Not enough points left\nYou have {statsPoints} left")
+                addStatsPoints(player, statsPoints)
+        if statsPoints == 0:
+            menu(player)
+        print(player)
+
+
+def main():
+    clear_screen()
+    characterInput()
+
+
+main()
