e62a2a997a7a21ca7fca09aeb694b6fa4a4d085b
Johan Plym Arkert <johan.arkert@gmail.com>
2023-09-06 08:12:30 +0200

    Google API

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
-import random, pprint, os, time
+import random, pprint, os, time, gspread
+from google.oauth2.service_account import Credentials
 
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("enemy")
+
+enemy = SHEET)
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
