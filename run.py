e48988ac27bfa110330a50cbf2decffb4b5011ba
Johan Plym Arkert <johan.arkert@gmail.com>
2023-09-07 12:04:10 +0200

    get opponent menu()

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import random, pprint, os, time, gspread, csv
+import random, pprint, os, time, gspread, csv, openai
 from google.oauth2.service_account import Credentials
 import pandas as pd
 
@@ -12,14 +12,7 @@ CREDS = Credentials.from_service_account_file("cred.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("enemy").sheet1
-
-df = pd.read_csv(SHEET)
-
-# Delete the last row using slicing
-df = df[:-1]
-
-# Save the modified DataFrame to the CSV file
-df.to_csv("SHEET", index=False)
+openai.api_key_path = "cred.txt"
 
 
 def clear_screen():
@@ -27,38 +20,79 @@ def clear_screen():
 
 
 def menu(player):
-    print("Choose your next opponent")
+    menu = {}
+    menu["1"] = "Create your Hero"
+    menu["2"] = "View Stats"
+    menu["3"] = "Choose Opponent"
+    menu["4"] = "View Wins"
+    menu["5"] = "Quit"
+
+    while True:
+        options = menu.keys()
+        options = sorted(options)
+        for entry in options:
+            print(entry, menu[entry])
+        selection = input("Please select an option: ")
+        if selection == "1":
+            characterInput()
+        elif selection == "2":
+            print(player)
+        elif selection == "3":
+            enemy, healthPoints = getEnemy(opponentsLst())
+            story(player, enemy)
+            swordBattle(player, enemy, healthPoints)
+        elif selection == "4":
+            print("You selected 'View Wins'.")
+        elif selection == "5":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option selected. Please try again.")
+
+
+def opponentsLst():
+    csvLst = SHEET.get_all_values()
+    x = 0
+    for row in csvLst:
+        if x != 0 and row[3] != "0":
+            print(f"{x}. {row[1]}")
+            x += 1
+        else:
+            x += 1
+    opponent = input("Please select an option: ")
+    return int(opponent) + 1
 
 
 def swordBattle(player, enemy, healthPoints):
     clear_screen()
+    print(f"\t\t⚔⚔⚔---Battle---⚔⚔⚔")
     while True:
-        clear_screen
-        print(f"\t\t⚔⚔⚔---Battle---⚔⚔⚔")
         attack = (player.skillPoints + dice(3)) - (enemy.skillPoints + dice(3))
         time.sleep(1)
         if attack == 0:
             print(f"The swords clash and no damage is dealt to either opponent")
         if attack > 0:
             damage = attack + player.strengthPoints - player.armor
-            print(f"{player.name} strikes {enemy.name} who looses {damage} HEALTH")
+            print(f"{player.name} strikes {enemy.name} who looses {damage} HP")
             enemy.healthPoints -= damage
-
+            print(f"{enemy.name} now has {enemy.healthPoints} HP left")
             if enemy.healthPoints < 1:
                 print(
                     f"{enemy.name.upper()} recieves a deadly blow. \n{player.name.upper()} lifts the sword in triumph"
                 )
                 dead = SHEET.row_values(2)
                 dead[3] = 0
+
                 SHEET.append_row(dead)
 
                 break
         if attack < 0:
             damage = abs(attack) + enemy.strengthPoints - player.armor
             print(
-                f"{player.name.upper()} strikes {enemy.name.upper()} who looses {damage} HEALTH"
+                f"{enemy.name.upper()} strikes {player.name.upper()} who looses {damage} HP"
             )
-            enemy.healthPoints -= damage
+            player.healthPoints -= damage
+            print(f"{player.name} now has {player.healthPoints} HP left")
             if player.healthPoints < 1:
                 print(
                     f"{enemy.name.upper()} recieves a deadly blow. \n{player.name.upper()} lifts sword in triumph"
@@ -96,47 +130,53 @@ def characterInput():
     name = input("Choose your characters name\n")
     time.sleep(1)
     clear_screen()
-    type = input("Choose your characters type\nHuman/Elf/Dwarf/Orc\n").lower()
+    typeChoice = input(
+        "Choose your characters type\n1. Human\n2. Elf\n3. Dwarf\n4. Orc\n"
+    ).lower()
     time.sleep(1)
     clear_screen()
-    if type == "human":
-        strengthPoints = 15 + dice(6)
-        healthPoints = 15 + dice(6)
-        skillPoints = 15 + dice(6)
+    if typeChoice == "1" or "human":
+        type = "human"
+        strengthPoints = 5 + dice(3)
+        healthPoints = 5 + dice(3)
+        skillPoints = 5 + dice(3)
+        armor = 5
+        statsPoints = dice(7)
+        player = CharacterStats(
+            type, name, strengthPoints, healthPoints, skillPoints, armor
+        )
+    if typeChoice == "2" or "elf":
+        type = "elf"
+        strengthPoints = 0 + dice(3)
+        healthPoints = 0 + dice(3)
+        skillPoints = 10 + dice(3)
         armor = 5
         statsPoints = dice(10)
         player = CharacterStats(
             type, name, strengthPoints, healthPoints, skillPoints, armor
         )
-    if type == "elf":
-        strengthPoints = 15 + dice(6)
-        healthPoints = 15 + dice(6)
-        skillPoints = 15 + dice(6)
-        armor = 5
-        statsPoints = dice(9)
+    if typeChoice == "3" or "dwarf":
+        type = "dwarf"
+        strengthPoints = 10 + dice(3)
+        healthPoints = 0 + dice(3)
+        skillPoints = 0 + dice(3)
+        armor = 15
+        statsPoints = dice(7)
         player = CharacterStats(
             type, name, strengthPoints, healthPoints, skillPoints, armor
         )
-    if type == "dwarf":
-        strengthPoints = 15 + dice(6)
-        healthPoints = 15 + dice(6)
-        skillPoints = 15 + dice(6)
-        armor = 5
-        statsPoints = dice(10)
-        player = CharacterStats(
-            type, name, strengthPoints, healthPoints, skillPoints, armor
-        )
-    if type == "orc":
-        strengthPoints = 15 + dice(6)
-        healthPoints = 15 + dice(6)
-        skillPoints = 15 + dice(6)
-        armor = 5
-        statsPoints = dice(10)
+    if typeChoice == "4" or "Orc":
+        type = "orc"
+        strengthPoints = 10 + dice(6)
+        healthPoints = 0 + dice(6)
+        skillPoints = 0 + dice(6)
+        armor = 10
+        statsPoints = dice(7)
         player = CharacterStats(
             type, name, strengthPoints, healthPoints, skillPoints, armor
         )
     else:
-        print(f"Choices available are Human/Elf/Dwarf/Orc\nYou wrote {name}")
+        print(f"Choices available are for Human/Elf/Dwarf/Orc\nYou wrote {name}")
 
     print(player)
     addStatsPoints(player, statsPoints)
@@ -152,17 +192,6 @@ def addStatsPoints(player, statsPoints):
             f"What stats would you like to improve?\n1. STRENGTH:\t{getattr(player, 'strengthPoints')}\n2. HEALTH:\t{getattr(player, 'healthPoints')}\n3. SWORD SKILL:\t{getattr(player, 'skillPoints')}\n4. ARMOR:\t{getattr(player, 'armor')}\n"
         )
         if statsPoints < 1:
-            enemyVals = SHEET.row_values(2)
-            type = enemyVals[0]
-            name = enemyVals[1]
-            strengthPoints = int(enemyVals[2])
-            healthPoints = int(enemyVals[3])
-            skillPoints = int(enemyVals[4])
-            armor = int(enemyVals[5])
-            enemy = CharacterStats(
-                type, name, strengthPoints, healthPoints, skillPoints, armor
-            )
-            swordBattle(player, enemy, enemyVals[3])
             menu(player)
         selectAttribute = input(f"Choose attribute 1/2/3/4: ")
         if selectAttribute == "1":
@@ -201,9 +230,46 @@ def addStatsPoints(player, statsPoints):
         print(player)
 
 
+def getEnemy(num):
+    enemyVals = SHEET.row_values(num)
+    type = enemyVals[0]
+    name = enemyVals[1]
+    strengthPoints = int(enemyVals[2])
+    healthPoints = int(enemyVals[3])
+    skillPoints = int(enemyVals[4])
+    armor = int(enemyVals[5])
+    enemy = CharacterStats(type, name, strengthPoints, healthPoints, skillPoints, armor)
+    return enemy, enemyVals[3]
+
+
+def story(player, enemy):
+    messages = [
+        {"role": "system", "content": "You are a Storyteller"},
+    ]
+    message = f" {player.name} the {player.type} and {enemy.name} the {enemy.type} draws swords against eachother. A battle between them is to start when the story ends. Maximum length 60 words"
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    reply = chat.choices[0].message.content
+    print(f"A Lord of the Strings tale: {reply}")
+    messages.append({"role": "assistant", "content": reply})
+    startBattle = input(f"\nPress Enter to start the battle")
+
+
 def main():
     clear_screen()
-    characterInput()
+    print(
+        "\t\tWelcome to LORD OF THE STRINGS\nA RPG-adventure game powered by the story-telling of chat-gpt\n\t\t      Now enter the realm"
+    )
+    player = "Hero has not been created"
+
+    time.sleep(1)
+    menu(
+        player,
+    )
 
 
 main()
